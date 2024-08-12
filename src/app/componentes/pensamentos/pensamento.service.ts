5c2b06af9d0ebbe2a2d456b8b64add6c7fa0de50
btwSanToS <andrelmsantos.87@gmail.com>
2024-08-12 08:13:54 -0300

    Criado observables para comunicação entre as páginas e criação dos pensamentos enviadas para o DB. Modificado as funções de criar e listar Pensamentos. Após criar, enviadas ao DB e redirecionadas para a página /ListarPensamento. Para o botão de cancelar, tirado a mensagem e criado a rota de direcionamento para /listarPensamento

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Pensamento } from './pensamento';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,11 @@ export class PensamentoService {
   private readonly API = 'http://localhost:3000/pensamentos'
   constructor(private http: HttpClient) { }
 
-  listar() {
+  listar(): Observable<Pensamento[]> {
     return this.http.get<Pensamento[]>(this.API)
   }
-  
+
+  criar(pensamento: Pensamento): Observable<Pensamento> {
+    return this.http.post<Pensamento>(this.API, pensamento)
+  }
 }
